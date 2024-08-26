e70cd25b1242fc4dcafd169c96d3a87f70ffd3fa
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-08-27 01:43:21 +0200

    implemented ticket types

--- database/migrations/2024_08_26_231928_create_ticket_types_table.php
+++ database/migrations/2024_08_26_231928_create_ticket_types_table.php
@@ -4,24 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTicketTypesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('ticket_types', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->text('benefits')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('ticket_types');
     }
-};
+}
