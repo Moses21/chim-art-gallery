8073dd2ce72ce586552ca8605078583e19d33b03
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-08-27 01:41:20 +0200

    events table migration file created

--- database/migrations/2024_08_26_231337_create_events_table.php
+++ database/migrations/2024_08_26_231337_create_events_table.php
@@ -4,24 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateEventsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date')->nullable();
+            $table->string('location');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('events');
     }
-};
+}
