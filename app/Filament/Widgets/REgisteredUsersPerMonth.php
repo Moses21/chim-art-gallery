a6ac720029527dc18d03486f893280606e791bee
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-08-27 02:24:05 +0200

    added barchart on registered users

--- app/Filament/Widgets/REgisteredUsersPerMonth.php
+++ app/Filament/Widgets/REgisteredUsersPerMonth.php
@@ -2,16 +2,35 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
 
-class REgisteredUsersPerMonth extends ChartWidget
+class RegisteredUsersPerMonth extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+
+
+   protected static ?string $heading = 'Users Registered Per Month';
 
     protected function getData(): array
     {
+        $usersPerMonth = User::select(
+            DB::raw("COUNT(*) as count"),
+            DB::raw("DATE_FORMAT(created_at, '%Y-%m') as month")
+        )
+        ->groupBy('month')
+        ->orderBy('month')
+        ->get()
+        ->pluck('count', 'month');
+
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Users Registered',
+                    'data' => $usersPerMonth->values(),
+                ],
+            ],
+            'labels' => $usersPerMonth->keys(),
         ];
     }
 
