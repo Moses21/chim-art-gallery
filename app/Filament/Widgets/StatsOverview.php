00b47d6b9ce6626b938859a8e21452cdf114e8c0
Takondwa-tebac <takondwakapyolah@gmail.com>
2024-08-27 02:33:23 +0200

    implemented tickets overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,7 +5,7 @@ namespace App\Filament\Widgets;
 use App\Models\User;
 use App\Models\Event;
 use App\Models\Items;
-use PHPUnit\Framework\Attributes\Ticket;
+use App\Models\EventTicket;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -18,9 +18,9 @@ class StatsOverview extends BaseWidget
         return [
             Stat::make('Total Users', User::count()),
             Stat::make('Total Artworks added', Items::count()),
-            Stat::make('Total Tickets Sold', Ticket::count()),
+            Stat::make('Total Tickets Sold', EventTicket::count()),
             Stat::make('Total Events', Event::count()),
-            Stat::make('Total Revenue', 'MWK ' . number_format(Ticket::sum('price'))),
+            Stat::make('Total Revenue', 'MWK ' . number_format(EventTicket::sum('price'))),
 
 
         ];
